00036bf497709d56300d6f45545996e3c4048d9f
pranta-barua007 <prantabarua247@gmail.com>
2022-08-04 21:43:00 +0600

    signup & auth flow

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -34,6 +35,15 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, process.env.JWT_KEY!);
+
+    req.session = {
+      jwt: userJwt
+    }
 
     res.status(201).send(user);
   }
--- auth/src/server.ts
+++ auth/src/server.ts
@@ -8,9 +8,16 @@ import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 const app = express();
+
+app.set('trust proxy', true); //handle https proxy of ngnx
 app.use(express.json());
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}))
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -24,15 +31,18 @@ app.all("*", async () => {
 app.use(errorHandler);
 
 const start = async () => {
-    try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-    }catch(err) {
-        console.error(err)
-    }
+  if (!process.env.JWT_KEY) {
+    throw new Error('Jwt must be defined')
+  }
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  } catch (err) {
+    console.error(err)
+  }
 
-    app.listen(3000, () => {
-      console.log("Listening on port 3000!!!!!!!!");
-    });
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!!!");
+  });
 }
 
 start();
