295a4233307d162924753e404bc8bc00706c77c1
pranta-barua007 <prantabarua247@gmail.com>
2022-09-12 12:12:26 +0600

    updated ticket service with proper versioning

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -30,7 +30,8 @@ router.post(
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
-      userId: ticket.userId
+      userId: ticket.userId,
+      version: ticket.version
     });
 
     res.status(201).send(ticket);
