53f028615b77395bfdf56887db815f18f8855494
pranta-barua007 <prantabarua247@gmail.com>
2022-09-20 20:25:04 +0600

    wired up expiration service with order service

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,33 @@
+import { Listener, Subjects, ExpirationCompleteEvent, OrderStatus } from "@prbtickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publisher/order-cancelled-publisher";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+    subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+        const order = await Order.findById(data.orderId).populate('ticket');
+
+        if(!order) {
+            throw new Error('Order not found');
+        }
+
+        order.set({
+            status: OrderStatus.Cancelled
+        });
+        await order.save();
+
+        new OrderCancelledPublisher(this.client).publish({
+            id: order.id,
+            version: order.version,
+            ticket: {
+                id: order.ticket.id
+            }
+        });
+
+        msg.ack();
+    }
+}
--- orders/src/server.ts
+++ orders/src/server.ts
@@ -3,6 +3,7 @@ import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
 import { TicketCreatedListener } from "./events/listeners/ticket-created-listner";
 import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listenr";
+import { ExpirationCompleteListener } from "./events/listeners/expiration-complete-listener";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -40,6 +41,7 @@ const start = async () => {
 
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
+    new ExpirationCompleteListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     mongoose.connection.on('open', () => {
