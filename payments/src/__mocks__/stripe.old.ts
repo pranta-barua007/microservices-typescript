842624764db397d43046c77f38de9f4d8875d2cb
pranta-barua007 <prantabarua247@gmail.com>
2022-09-23 00:09:59 +0600

    updated payment service

--- payments/src/__mocks__/stripe.old.ts
+++ payments/src/__mocks__/stripe.old.ts
@@ -1,3 +1,4 @@
+//this implementation is not used, rather we're connecting to real stripe service and testing it
 export const stripe = {
   charges: {
     create: jest.fn().mockResolvedValue({}), //mock resoleve returns a promise which will aleays resolve
