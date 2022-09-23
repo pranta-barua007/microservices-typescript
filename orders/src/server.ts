12336bbb0d6d46e188fbf754f8563ccbb5abeec1
pranta-barua007 <prantabarua247@gmail.com>
2022-09-24 00:21:19 +0600

    updated test workflows

--- orders/src/server.ts
+++ orders/src/server.ts
@@ -7,6 +7,8 @@ import { ExpirationCompleteListener } from "./events/listeners/expiration-comple
 import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 
 const start = async () => {
+  console.log('starting orders ...')
+
   if (!process.env.JWT_KEY) {
     throw new Error('Jwt must be defined')
   }
