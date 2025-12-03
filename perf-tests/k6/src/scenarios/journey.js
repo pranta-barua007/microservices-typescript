ab25f1be3e84d969b92b70a7bdeaf3857bdd0fc0
pranta-barua007 <prantabarua247@gmail.com>
2025-12-03 13:31:37 +0600

    fix: payment passign after successful integration by intoducing delay of 1second between requests'

--- perf-tests/k6/src/scenarios/journey.js
+++ perf-tests/k6/src/scenarios/journey.js
@@ -1,3 +1,4 @@
+import { sleep } from 'k6';
 import { ensureUserExists, loginUser } from "../lib/auth.js";
 import { createTicket } from "../lib/tickets.js";
 import { createOrder, getOrders } from "../lib/orders.js";
@@ -5,7 +6,7 @@ import { payOrder } from "../lib/payments.js";
 
 export const options = {
   vus: 20,
-  duration: "2m",
+  duration: "1m",
   insecureSkipTLSVerify: true,
 };
 
@@ -19,16 +20,23 @@ export default function (data) {
   const cookie = data.cookie;
 
   // 1. Create Ticket
-  const ticket = createTicket(cookie, "Journey Ticket", 40);
+  const ticketName = `Journey Ticket ${__ITER}`;
+  const ticketPrice = 40 + __ITER;
+  const ticket = createTicket(cookie, ticketName, ticketPrice);
   const ticketId = ticket.json().id;
+  
+  sleep(1); 
 
   // 2. Create Order
   const order = createOrder(cookie, ticketId);
   const orderId = order.json().id;
 
+  sleep(1);
+
   // 3. Pay Order
   payOrder(cookie, orderId);
 
+  sleep(1);
   // 4. Get all orders
   getOrders(cookie);
 }
